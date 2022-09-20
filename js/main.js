a190f5003197eb7139eb914ae21bb65fdb9d0771
achasek <chaseknieper@gmail.com>
2022-09-20 15:06:08 -0700

    'trying to change the idx values'

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 /*----- constants -----*/
-const winningCombos = [
-    //columns
-    [0, 5, 10, 15],
-    [1, 6, 11, 16],
-    [2, 7, 12, 17],
-    [3, 8, 13, 18],
-    [4, 9, 14, 19],
-    //rows left-aligned
-    [0, 1, 2, 3],
-    [5, 6, 7, 8],
-    [10, 11, 12, 13],
-    [15, 16, 17, 18],
-    //rows right-aligned
-    [1, 2, 3, 4],
-    [6, 7, 8, 9],
-    [11, 12, 13, 14],
-    [16, 17, 18, 19],
-    //diagonals
-    [0, 6, 12, 18],
-    [1, 7, 13, 19],
-    [3, 7, 11, 15],
-    [4, 8, 12, 16],
-];
+// const winningCombos = [
+//     //columns
+//     [0, 5, 10, 15],
+//     [1, 6, 11, 16],
+//     [2, 7, 12, 17],
+//     [3, 8, 13, 18],
+//     [4, 9, 14, 19],
+//     //rows left-aligned
+//     [0, 1, 2, 3],
+//     [5, 6, 7, 8],
+//     [10, 11, 12, 13],
+//     [15, 16, 17, 18],
+//     //rows right-aligned
+//     [1, 2, 3, 4],
+//     [6, 7, 8, 9],
+//     [11, 12, 13, 14],
+//     [16, 17, 18, 19],
+//     //diagonals
+//     [0, 6, 12, 18],
+//     [1, 7, 13, 19],
+//     [3, 7, 11, 15],
+//     [4, 8, 12, 16],
+// ];
 
 const PLAYER_LOOKUP = {
     '1': {
         name: 'Player1',
-        symbol: 'O',
+        color: 'red',
 
     },
     '-1': {
         name: 'Computer',
-        symbol: 'X',
+        color: 'yellow',
     },
 };
 
@@ -46,6 +46,7 @@ const messageDisplayEl = document.querySelector('h2');
 const resetBtnEl = document.getElementById('reset');
 // const cpuTurnEl = document.getElementById('CPU');
 const boardEl = document.getElementById('board');
+const squareEl = document.querySelector('.square');
 
 /*----- event listeners -----*/
 resetBtnEl.addEventListener('click', handleResetClick);
@@ -69,13 +70,22 @@ function handleResetClick() {
 // }
 
 function handleBoardClick(evt) {
-    console.log(evt)
+    console.log(evt.target.id)
     if(winner) { 
-        console.log(winner)
-        return
+        // console.log(winner)
+        return;
     }
+    // let coordinates = square.id.split('-');
+    // let row = parseInt(coordinates[0]);
+    // let column = parseInt(coordinates[1]);
     if (evt.target.id !== 'board') {
-        const idx = evt.target.id[2];
+        // const rowIdx = evt.target.id[0];
+        // const columnIdx = evt.target.id[2];
+        // const idx = 
+        let coordinates = board.id.split('-');
+        let row = parseInt(coordinates[0]);
+        let column = parseInt(coordinates[1]);
+        let idx = [row][column]
         if (!board[idx]) {
             board[idx] = turn;
             checkWin();
@@ -83,6 +93,7 @@ function handleBoardClick(evt) {
             render();
             // computerChooses();
         }
+        console.log(idx)
     };
 };
 
@@ -96,13 +107,22 @@ function render() {
    };
    
    board.forEach(function (square, idx) {
+    // playPiece();
     if(square) {
-        document.getElementById(`sq${idx}`).textContent = PLAYER_LOOKUP[square].symbol;
+        document.getElementById(`sq${idx}`).style.background = PLAYER_LOOKUP[square].color;
     } else {
-        document.getElementById(`sq${idx}`).textContent = '';
+        document.getElementById(`sq${idx}`).style.background = '';
     };
 });
 };
+
+// function playPiece() {
+//     if(turn === 1) {
+//         squareEl.style.background = 'red';
+//     } else if (turn === -1) {
+//         squareEl.style.background = 'yellow';
+//     }
+// }
 
 function changeTurn() {
     turn = turn * -1
