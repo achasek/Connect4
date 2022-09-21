4a6567f33b8d7f9f3e25830a876bb3267e36bb13
achasek <chaseknieper@gmail.com>
2022-09-21 12:13:10 -0700

    set pieces, alternate turns, gravity of pieces

--- js/main.js
+++ js/main.js
@@ -1,148 +1,108 @@
 /*----- constants -----*/
-// const winningCombos = [
-//     //columns
-//     [0, 5, 10, 15],
-//     [1, 6, 11, 16],
-//     [2, 7, 12, 17],
-//     [3, 8, 13, 18],
-//     [4, 9, 14, 19],
-//     //rows left-aligned
-//     [0, 1, 2, 3],
-//     [5, 6, 7, 8],
-//     [10, 11, 12, 13],
-//     [15, 16, 17, 18],
-//     //rows right-aligned
-//     [1, 2, 3, 4],
-//     [6, 7, 8, 9],
-//     [11, 12, 13, 14],
-//     [16, 17, 18, 19],
-//     //diagonals
-//     [0, 6, 12, 18],
-//     [1, 7, 13, 19],
-//     [3, 7, 11, 15],
-//     [4, 8, 12, 16],
-// ];
-
 const PLAYER_LOOKUP = {
     '1': {
-        name: 'Player1',
+        name: 'player1',
         color: 'red',
-
     },
     '-1': {
-        name: 'Computer',
+        name: 'computer',
         color: 'yellow',
     },
 };
 
-const boardSize = 20;
-
-/*----- app's state (variables) -----*/ 
+/*----- app's state (variables) -----*/
 let turn, board, winner;
 
 
+
 /*----- cached element references -----*/ 
-const messageDisplayEl = document.querySelector('h2');
-const resetBtnEl = document.getElementById('reset');
-// const cpuTurnEl = document.getElementById('CPU');
-const boardEl = document.getElementById('board');
-const squareEl = document.querySelector('.square');
+let messageDisplayEl = document.querySelector('h2');
+let resetBtnEl = document.getElementById('reset');
+let boardEl = document.getElementById('board');
+
+
 
 /*----- event listeners -----*/
 resetBtnEl.addEventListener('click', handleResetClick);
-boardEl.addEventListener('click', handleBoardClick);
-// cpuTurnEl.addEventListener('click', handleCpuClick);
+boardEl.addEventListener('click', handleBoardClick); 
+
+
+
 
 /*----- functions -----*/
 function init() {
-    turn = 1;
-    board = [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null];
-    winner = null;
+    turn = 1
+    board = [
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+        [null, null, null, null, null],
+    ]
+    columns = [3, 3, 3, 3, 3]
+    winner = null
     render();
+};
+
+function handleBoardClick(evt) {
+    // console.log(evt.target.id)
+    if(winner) {
+        return
+    }
+    if(evt.target.id !== 'board') {
+        let rowIdx = evt.target.id[0]
+        let columnIdx = evt.target.id[2]
+        console.log(rowIdx, columnIdx)
+        rowIdx = columns[columnIdx];
+        if (rowIdx < 0) {
+            return;
+        }
+        
+        if(!board[rowIdx][columnIdx]) {
+            board[rowIdx][columnIdx] = turn
+            checkWin();
+            changeTurn();
+            render();
+        }
+
+        rowIdx -= 1;
+        columns[columnIdx] = rowIdx;
+    }
 };
 
 function handleResetClick() {
     init();
 };
 
-// function handleCPUClick() {
-    
-// }
-
-function handleBoardClick(evt) {
-    console.log(evt.target.id)
-    if(winner) { 
-        // console.log(winner)
-        return;
-    }
-    // let coordinates = square.id.split('-');
-    // let row = parseInt(coordinates[0]);
-    // let column = parseInt(coordinates[1]);
-    if (evt.target.id !== 'board') {
-        // const rowIdx = evt.target.id[0];
-        // const columnIdx = evt.target.id[2];
-        // const idx = 
-        let coordinates = board.id.split('-');
-        let row = parseInt(coordinates[0]);
-        let column = parseInt(coordinates[1]);
-        let idx = [row][column]
-        if (!board[idx]) {
-            board[idx] = turn;
-            checkWin();
-            changeTurn()
-            render();
-            // computerChooses();
-        }
-        console.log(idx)
-    };
+function changeTurn() {
+    turn = turn * -1;
 };
 
 function render() {
-   if (!winner && board.every(square => square)) {
-    (messageDisplayEl.innerHTML = "It's a Tie!")
-   } else if (!winner) {
-    (messageDisplayEl.innerHTML = `${PLAYER_LOOKUP[turn].name}'s turn`)
-   } else {
-    (messageDisplayEl.innerHTML = `${PLAYER_LOOKUP[winner].name} Wins!`)
-   };
-   
-   board.forEach(function (square, idx) {
-    // playPiece();
-    if(square) {
-        document.getElementById(`sq${idx}`).style.background = PLAYER_LOOKUP[square].color;
-    } else {
-        document.getElementById(`sq${idx}`).style.background = '';
-    };
-});
-};
-
-// function playPiece() {
-//     if(turn === 1) {
-//         squareEl.style.background = 'red';
-//     } else if (turn === -1) {
-//         squareEl.style.background = 'yellow';
-//     }
-// }
-
-function changeTurn() {
-    turn = turn * -1
-}
-
-function checkWin() {
-    for (let combo of winningCombos) {
-        let totalToWin = 0;
-        combo.forEach(idx => {
-            totalToWin += board[idx]
-        })
-        if (Math.abs(totalToWin)=== 4) {
-            winner = turn;
-            break
+    // if its tie, win, or currently someones turn
+    if(!winner) {
+        if (board.forEach((row, idx) => {
+             true
+            }) {
+            messageDisplayEl.innerHTML = "It's a tie!";
+            }
         }
     }
+
+    board.forEach((row, idx) => {
+        row.forEach((square, idy) => {
+        if (square) {
+            document.getElementById(`${idx}-${idy}`).style.backgroundColor = PLAYER_LOOKUP[square].color 
+        } else {
+            document.getElementById(`${idx}-${idy}`).style.backgroundColor = 'white';
+        }
+    })
+    });
 };
 
-// function computerChooses() {
-//     const randomIdx = Math.floor(Math.random() * board.length);
-//     computer.choice = board[randomIdx];
-// };
+function checkWin() {
+    //google sliding window in cs
+};
+
+
+
 init();
